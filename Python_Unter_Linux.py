aee6ec9e612424aec6ab9198284ef16d99fb6c8c
yi-joseph <yimiaoheidelberg@gmail.com>
2025-10-02 15:51:23 +0200

    Update Python_Unter_Linux.py

--- Python_Unter_Linux.py
+++ Python_Unter_Linux.py
@@ -1,1 +1,10 @@
-# 
+# 1
+# Ich möchte p für "python" abkürzen: 
+nano ~/.zshrc
+# Dann schreibe ich folgende Zeile auf: 
+alias p='python3'
+# Speichern: strg + o
+# Exit: strg + x
+# Die Datei aktivieren
+source ~/.zshrc
+
